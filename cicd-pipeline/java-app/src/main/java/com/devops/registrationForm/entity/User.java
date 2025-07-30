709838db15ae16866518bb67dc8731d6c37e1e9e
kavyak-2305 <kavyakuthala2305@example.com>
2025-07-30 14:54:00 +0530

    organised

---
+++ cicd-pipeline/java-app/src/main/java/com/devops/registrationForm/entity/User.java
@@ -0,0 +1,15 @@
+package com.devops.registrationForm.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String email;
+    private String phone;
+    private String interest;
+}
