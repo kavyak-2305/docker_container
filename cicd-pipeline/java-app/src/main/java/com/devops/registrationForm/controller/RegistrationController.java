6e7bfaf470c2c2545d8a7bd21fd2b69df690a7bb
kavyak-2305 <kavyakuthala2305@example.com>
2025-08-02 22:01:00 +0530

    modified

--- cicd-pipeline/java-app/src/main/java/com/devops/registrationForm/controller/RegistrationController.java
+++ cicd-pipeline/java-app/src/main/java/com/devops/registrationForm/controller/RegistrationController.java
@@ -1,11 +1,8 @@
 package com.devops.registrationForm.controller;
 
 import com.devops.registrationForm.entity.User;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
